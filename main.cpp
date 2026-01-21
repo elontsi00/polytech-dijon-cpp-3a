768c80e36049d7e9bcf85bdd8d3c707fbedfb35b
elontsi00 <elontsioo@gmail.com>
2026-01-21 17:14:20 +0100

    deuxieme

--- main.cpp
+++ main.cpp
@@ -10,18 +10,30 @@
 
 
 int main() {
+// creation materiel
     Materiel m1("Dell OptiPlex", TypeMateriel::poste_travail);
+    Materiel m2("Ecran Samsung", TypeMateriel::ecran);
+    Materiel m3("Switch Cisco", TypeMateriel::materiel_reseau);
 
-    // reservation materiel
+// creation salles
+    Salle salle1("Salle 101", TypeSalle::informatique);
+    Salle salle2("Salle 202", TypeSalle::technique);
+    Salle salle3("Salle 303", TypeSalle::cours);
+// ajouter materiel aux salles
+    salle1.ajouterMateriel(&m1);
+    salle3.ajouterMateriel(&m2);
+    salle2.ajouterMateriel(&m3);
+
+  // afficher inventaire des salles  
+    salle1.afficherInventaire();
+    salle2.afficherInventaire();
+    salle3.afficherInventaire();
+
+// reservation materiel
     std::cout << m1 << std::endl;
     Date dateDebut(15, 6, 2024);
     Date dateFin(20, 6, 2024);
     m1.reserver_materiel("Dell OptiPlex", dateDebut, dateFin);
-
-
-    Salle salle1("Salle 101", TypeSalle::informatique);
-    salle1.ajouterMateriel(&m1);
-    salle1.afficherInventaire();
 
     return 0;
 
